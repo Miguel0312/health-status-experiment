0f9f7834d4f50c552ad9022fc4cde96ea1be89dd
miguel <miguel.vpereira14@gmail.com>
2025-04-25 16:12:48 +0200

    Add the change rates to the data frames

--- main.py
+++ main.py
@@ -2,9 +2,25 @@ import pandas as pd
 import logging
 
 #TODO: improve configuration
-NUMBER_OF_SAMPLES = 24
+NUMBER_OF_SAMPLES = 8
 DATA_FILE = "data/mini-baidu-dataset.csv"
 # DATA_FILE = "data/baidu-dataset.csv"
+CHANGE_RATE_INTERVAL = 6
+
+def computeChangeRates(df, interval):
+	# Remove the serial number
+	ratesColumns = list(df.columns)[1:]
+	titles = [column + " Change Rate" for column in ratesColumns]
+	for column in ratesColumns:
+		df = df.assign(column = [None for i in range(len(df))])
+
+	for i in range(interval, len(df)):
+		if df.iloc[i]["serial-number"] != df.iloc[i-interval]["serial-number"]:
+			continue
+		for idx, column in enumerate(ratesColumns):
+			df.at[i, titles[idx]] = (df.iloc[i][column] - df.iloc[i-interval][column])/interval
+
+	return df
 
 # Keeps only the last N samples of each disk on the dataframe
 def getLastSamples(df, N):
@@ -28,13 +44,17 @@ data = pd.read_csv("data/mini-baidu-dataset.csv")
 good_hard_drives = data[data["Drive Status"] == 1]
 bad_hard_drives = data[data["Drive Status"] == -1]
 
-#print(good_hard_drives)
-#print(bad_hard_drives)
+# TODO: check how the normalization is done
+good_hard_drives = computeChangeRates(good_hard_drives, CHANGE_RATE_INTERVAL)
+bad_hard_drives = computeChangeRates(bad_hard_drives, CHANGE_RATE_INTERVAL)
 
-bad_hard_drives = getLastSamples(bad_hard_drives, 8)
+bad_hard_drives = getLastSamples(bad_hard_drives, NUMBER_OF_SAMPLES)
+
+#TODO: filter columns (z-scores, maybes)
+
+print(list(bad_hard_drives.columns))
 print(bad_hard_drives)
 
-# TODO: add the rate of change to the table (read how the current reearch does it: hour by hour or compares to another basis)
 
 # TODO: creating training and test samples 
 
